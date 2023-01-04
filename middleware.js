23a405fbd0b2e41a571b86ce34bf5e93ce72eee7
jstgrowup <deysubham999@gmail.com>
2023-01-04 18:20:42 +0530

    middleware issue

--- middleware.js
+++ middleware.js
@@ -3,7 +3,7 @@ export default function middleware(req) {
   let verify = req.cookies.get("loggedin");
 
   if (verify === undefined || !verify.value) {
-    return NextResponse.redirect("http://localhost:3000/signin");
+    return NextResponse.redirect("/signin");
   } else {
     return NextResponse.next();
   }
